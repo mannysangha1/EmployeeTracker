ec2b1b571f8582071d6891a0a35ae265e7ce1b89
Manny <mannysangha020@gmail.com>
2022-09-28 02:10:36 -0700

    added additional code

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ const Employee = require('./constructors/employee')
 const Role = require('./constructors/role')
 
 var connection = mysql.createConnection({
-    
-})
+    host: "localhost",
+
+    //port 
+
+    port: 3001,
+
+    //username
+
+    username: "root",
+
+    //password
+    password: "password",
+    database: "employee_db"
+});
