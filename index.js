23bdc5dbbcf24561470441ee071f5dafe19d6cf5
Manny <mannysangha020@gmail.com>
2022-10-06 01:23:24 -0700

    added some more code

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ var connection = mysql.createConnection({
     password: "password",
     database: "employee_db"
 });
+
+connection.connect(function(err) {
+    if (err) throw err; 
+    console.log("connected as id " + connection.threadId);
+});
+
+init ();
+
