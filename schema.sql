2dd44cce2b908fbd1582795989c53738f372e6b9
Manny <mannysangha020@gmail.com>
2022-10-14 02:14:17 -0700

    added CREATE TABLE department and employee

--- schema.sql
+++ schema.sql
@@ -6,6 +6,12 @@ USE employee_db;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT NOT NULL,
