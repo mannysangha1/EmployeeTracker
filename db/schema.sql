5c62ef03b4d9beaecebff91384d4174d91422510
Manny <mannysangha020@gmail.com>
2022-10-14 02:06:51 -0700

    added additional code

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,14 @@
 DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT DEFAULT NULL,
+    PRIMARY KEY (id)
+);
